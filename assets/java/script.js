74de955c52ecefdd9f3108ea4efb9731ecb9f708
LaLacou <108777796+users.noreply.github.com>
2022-08-08 22:30:21 -0700

    added form field for player to enter name

--- assets/java/script.js
+++ assets/java/script.js
@@ -1,6 +1,9 @@
-let time = 60;
+let time = 30;
 let timeId;
 let qI = 0;
+
+const formEl = document.querySelector('#score-name');
+const scoreNameEl = document.querySelector('#score-names')
 
 const main = document.querySelector('main');
 
@@ -17,11 +20,13 @@ const handleClock = () => {
     document.querySelector('.time').innerHTML = time;
 
 };
+//starts the game and the timer count down
 const init = () => {
     reinit(0);
-    
- };
 
+};
+
+//put the next questions on the page
 const reinit = (index) => {
 
     let { Q, A, C } = questions[index];
@@ -38,35 +43,57 @@ const reinit = (index) => {
 
 };
 
+
 var handleAns = (C, ans) => {
     if (C !== ans) {
         time -= 5;
     }
     else {
-      // calculateScore();
+        // calculateScore();
     }
-    getNewQuestions ();
+    getNewQuestions();
 };
 
-const maxQuestions = 5
+//calls the next questions
 getNewQuestions = () => {
     qI++;
     if (qI < questions.length) {
         reinit(qI);
     }
     else {
-       showFinalScore();
+        showFinalScore();
     }
 };
 
-var calculateScore = function(event) {
+//calculates how many questions the player got correct
+var calculateScore = function (event) {
 
 }
+//takes player to score page & will show final score
 var showFinalScore = () => {
-   if (time == 0 || qI == questions.length) {
-    window.location.href ='highscores.html' 
-   }
+    if (time == 0 || qI == questions.length) {
+        window.location.href = 'highscores.html'
+    }
 };
+
+//allows player to input name to save score
+var nameFormHandler = function (event) {
+    event.preventDefault();
+    var nameInput = document.querySelector("#name-form").value;
+    // check if inputs are empty (validate)
+    if (!nameInput) {
+        alert("You need to fill out the form!");
+        return false;
+    }
+    else {
+        var scoreDataObj = {
+            name: nameInput,
+        };
+        createScoreEl(scoreDataObj);
+    }
+};
+
+formEl.addEventListener('submit', nameFormHandler);
 document.querySelector('.start').addEventListener("click", init);
 
 
