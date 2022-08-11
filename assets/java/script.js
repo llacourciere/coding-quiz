55b3d42e045961db422715c0dc9676d91c966e97
LaLacou <108777796+users.noreply.github.com>
2022-08-10 21:51:49 -0700

    added showScores feature

--- assets/java/script.js
+++ assets/java/script.js
@@ -2,20 +2,18 @@ let time = 60;
 let timeId;
 let qI = 0;
 var scores = [];
-const formEl = document.querySelector('#score-name');
-const scoreNameEl = document.querySelector('#score-names')
+
 
 const main = document.querySelector('main');
-//var scores = [];
 
 const handleClock = () => {
 
     time--;
-        
+
     if (time < 1) {
         clearInterval(timeId);
         time = 0;
-        return showFinalScore();
+        return showEnterScore();
     };
     document.querySelector('.time').innerHTML = time;
 };
@@ -45,10 +43,10 @@ const reinit = (index) => {
 var handleAns = (C, ans) => {
     if (C !== ans) {
         time -= 5;
-        main.innerHTML = `<p>Wrong Answer!<p>`
+        //main.innerHTML = `<p>Wrong Answer!<p>`
     }
     else {
-        main.innerHTML = `<p>Right Answer!<p>`
+        //main.innerHTML = `<p>Right Answer!<p>`
     }
     getNewQuestions();
 };
@@ -60,18 +58,14 @@ getNewQuestions = () => {
         reinit(qI);
     }
     else {
-        showFinalScore();
+        showEnterScore();
     }
 };
 
-// var createScoreEl = function () {
-
-// }
-
-//takes player to score page & will show final score
-var showFinalScore = () => {
+//takes player to the enter name page
+var showEnterScore = () => {
     clearInterval(timeId);
-      document.body.innerHTML = `
+    document.body.innerHTML = `
       <nav>
       <a href="./index.html" ><button class="scores">Take Quiz Again</button></a>
       <h3>Highscores<span class="highscores"></span> </h3>
@@ -79,63 +73,86 @@ var showFinalScore = () => {
 
       <section id="high-score">
           <div id="score-name">
-              <input id="name-form" type="text" name="name" placeholder="Name">
+              <input id="name-form" type="text" name="name" placeholder="Initials">
               <button onclick="nameFormHandler()">Submit</button>
           </div>
-
-          <section class="high-score-list">
-              <ul onchange="loadScores()" class="score-list" id="score-names">
-                <li ></li>
-              </ul>
-          </section>
       </section>
       
       `;
-    
+
 };
+
+//enter the name and the score into an object when entered into the form
 var nameFormHandler = function (event) {
 
     var nameInput = document.querySelector("#name-form").value;
     // check if inputs are empty (validate)
-    
-    console.log(nameInput,': ',time);
+
+    console.log(nameInput, ': ', time);
 
     if (!nameInput) {
         alert("You need to fill out the form!");
         return false;
     }
-    else {
-        var scoreDataObj = {
-           name: nameInput,
-           score: time
-       };
-       scores = scoreDataObj;
-       saveScores();
+    var scoreDataObj = {
+        name: nameInput,
+        score: time
+    };
+    scores.push(scoreDataObj);
 
+    loadScores();
+    saveScores();
+    showScores(scores);
+};
+//show the most recent scores
+var showScores = function (scoreDataObj) {
+
+    document.body.innerHTML = `
+<nav>
+<a href="./index.html" ><button class="scores">Take Quiz Again</button></a>
+<h3>Highscores<span class="highscores"></span> </h3>
+</nav>
+
+<section id="high-score">
+    <section id="high-score-list">
+    
+    </section>
+</section>
+`
+var highscoreList = document.getElementById('high-score-list');
+console.log(highscoreList);
+    for (let i=0; i< scores.length; i++) {
+        var scoreItem = document.createElement('h4')
+        scoreItem.textContent = scores[i].name + " : " + scores[i].score 
+        highscoreList.appendChild(scoreItem);
+    }
+};
+//load and save scores to local storage
+var saveScores = function () {
+    scores.sort((a, b) => {
+        return a.score < b.score ? 1 : -1;
+    })
+    console.log(scores);
+    if (scores.length > 10) {
+        scores.pop()
+    }
+    localStorage.setItem("scores", JSON.stringify(scores));
+};
+var loadScores = function () {
+    var savedScores = localStorage.getItem("scores");
+
+    if (!savedScores) {
+        return false;
+    }
+    // parse into array of objects
+    savedScores = JSON.parse(savedScores);
+
+    for (var i = 0; i < savedScores.length; i++) {
+        scores.push(savedScores[i])
     }
 };
 
-//load and save scores to local storage
-var saveScores = function() {
-    localStorage.setItem("scores", JSON.stringify(scores));
-  };
-var loadScores = function() {
-    var savedScores = localStorage.getItem("scores");
-    // if there are no tasks, set tasks to an empty array and return out of the function
-    if (!savedScores) {
-      return false;
-    }
-    console.log("Saved scores found!");
-  
-    // parse into array of objects
-    savedScores= JSON.parse(savedScores);
-  
-    for (var i = 0; i < savedScores.length; i++) {
-      createScoreEl(savedScores[i]);
-    }
-  };
-
-document.querySelector('.start').addEventListener("click", init);  
+document.querySelector('.start').addEventListener("click", init);
 
 
 
